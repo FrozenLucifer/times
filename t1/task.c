5e9882d06c02d5da6d03fe62c05dfd34297afc1d
FrozenLucifer <67059921+FrozenLucifer@users.noreply.github.com>
2023-05-14 14:29:44 +0300

    Update task.c

--- t1/task.c
+++ t1/task.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
 #include <sys/time.h>
 #include <time.h>
-#define N 5
+#include <x86gprintrin.h>
+
+#define N 10
 #define TIME_1SEC {1, 0}
 #define TIME_100MSEC {0, 100000000}
 #define TIME_50MSEC {0, 50000000}
 #define TIME_10MSEC {0, 10000000}
+#define PROC_TICK 2800000000
 
+#define NS_TO_S 1000000000
+#define MCS_TO_S 1000000
 
 int nanosleep(const struct timespec *tw, struct timespec *tr);
 
@@ -14,7 +19,9 @@ int clock_gettime(clockid_t clockid, struct timespec *tp);
 
 clock_t clock(void);
 
+unsigned long long __rdtsc(void);
 
+    
 long long int get_by_gettimeofday(struct timespec t)
 {
     struct timeval start, end;
@@ -44,55 +51,85 @@ long long int get_by_clock(struct timespec t)
     start = clock();
     nanosleep(&t, &tmp);
     end = clock();
-    long long int delta = end - start; //
+    long long int delta = end - start;
+    return delta;
+}
+
+long long int get_by_rdtsc(struct timespec t)
+{
+    unsigned long long start, end;
+    struct timespec tmp;
+    start = __rdtsc();
+    nanosleep(&t, &tmp);
+    end = __rdtsc();
+    long long int delta = end - start;
     return delta;
 }
 
 void run_gettimeofday(struct timespec t)
 {
-    printf("%ld\n", CLOCKS_PER_SEC);
     printf("...gettimeofday...\n");
+    unsigned long long sum = 0;
     for (int i = 0; i < N; i++)
     {
-        printf("%lld mcs\n", get_by_gettimeofday(t));
+        sum += get_by_gettimeofday(t);
     }
+    printf("%lf s\n", (double)sum/N/MCS_TO_S);
 }
 
 
 void run_clock_gettime(struct timespec t)
 {
     printf("...clock_gettime...\n");
+    unsigned long long sum = 0;
     for (int i = 0; i < N; i++)
     {
-        printf("%lld ns\n", get_by_clock_gettime(t));
+        sum += get_by_clock_gettime(t);
     }
+    printf("%lf s\n", (double)sum/N/NS_TO_S);
 }
 
 
 void run_clock(struct timespec t)
 {
     printf("...clock...\n");
+    unsigned long long sum = 0;
+    for (int i = 0; i < N; i++)
+    {   
+        sum += get_by_clock(t);
+    }
+    printf("%lf ticks\n", (double)sum/N);
+}
+
+void run_rdtsc(struct timespec t)
+{
+    printf("...__rdtsc...\n");
+    unsigned long long sum = 0;
     for (int i = 0; i < N; i++)
     {
-        printf("%lld clocks\n", get_by_clock(t));
+        sum += get_by_rdtsc(t);
     }
+    printf("%lf s\n", (double)sum/N/PROC_TICK);
 }
 
 
 int main()
 {
+    struct timespec t = TIME_10MSEC, tim2;
 
-    struct timespec t = TIME_1SEC, tim2;
-    // long long int sum = 0;
-
-    // run_gettimeofday(t);
-
-    // run_clock_gettime(t);
-
-    run_clock(t);
+    struct timespec times[4] = {TIME_10MSEC, TIME_50MSEC, TIME_100MSEC, TIME_1SEC};
+    char strings[4][16] = {"TIME_10MSEC", "TIME_50MSEC", "TIME_100MSEC", "TIME_1SEC"};
+    for (int i = 0; i < 4; i++)
+    {
+        printf("%s\n", strings[i]);
+        run_gettimeofday(times[i]);
+    
+        run_clock_gettime(times[i]);
+    
+        run_clock(times[i]);
+        
+        run_rdtsc(times[i]);
+    }
 
     return 0;
 }
-
-
-
